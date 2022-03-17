c7dd0cec4f30347f3a0d78e2ae489616a42086e2
Maxim Petrenko <m.d.petrenko@gmail.com>
2022-03-17 16:23:01 +0500

    ProductConverter marked as Component

--- market-core/backend/src/main/java/com/github/mdpetrenko/market/core/backend/converters/ProductConverter.java
+++ market-core/backend/src/main/java/com/github/mdpetrenko/market/core/backend/converters/ProductConverter.java
@@ -2,7 +2,9 @@ package com.github.mdpetrenko.market.core.backend.converters;
 
 import com.github.mdpetrenko.market.core.api.dto.ProductDto;
 import com.github.mdpetrenko.market.core.backend.entities.Product;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ProductConverter{
     public ProductDto entityToDto(Product entity) {
         return new ProductDto(entity.getId(), entity.getTitle(), entity.getCategory().getTitle(), entity.getPrice());
