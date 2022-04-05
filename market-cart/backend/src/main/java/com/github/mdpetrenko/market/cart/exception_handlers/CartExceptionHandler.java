5ec7e4cc6dcbfecdd2b8dc2e21c8815b3a33d5dd
Maxim Petrenko <m.d.petrenko@gmail.com>
2022-04-05 15:13:24 +0500

    CartNotFoundException handler added

---
+++ market-cart/backend/src/main/java/com/github/mdpetrenko/market/cart/exception_handlers/CartExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.github.mdpetrenko.market.cart.exception_handlers;
+
+import com.github.mdpetrenko.market.cart.exceptions.CartError;
+import com.github.mdpetrenko.market.cart.exceptions.CartNotFoundException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+@Slf4j
+public class CartExceptionHandler {
+    @ExceptionHandler
+    public ResponseEntity<?> catchCartNotFoundException(CartNotFoundException e) {
+        return new ResponseEntity<>(new CartError(CartError.CartErrors.CART_NOT_FOUND.name(), e.getMessage()), HttpStatus.NOT_FOUND);
+    }
+}
