1ecf957797a79ec40f8f1ea2de31954d5aa502b8
maxx1088@gmail.com <pyvdup-6tyfno-mAppoc>
2022-04-12 14:33:02 +0500

    Added BillingAddress in User profile
    New page User profile created
    New table 'addresses' in users database
    New entity BillingAddress and dto
    Profile page in front added

---
+++ market-front/src/main/resources/static/user/profile/profile.js
@@ -0,0 +1,27 @@
+angular.module('market-front').controller('profileController', function ($scope, $http, $location) {
+    $scope.prefix = 'http://localhost:5555/auth/api/v1'
+
+
+    $scope.getUserInfo = function () {
+        $http.get($scope.prefix + '/user')
+            .then(function (response) {
+                $scope.userData = response.data;
+                console.log($scope.userData);
+            });
+    }
+
+    $scope.removeAddress = function (addressId) {
+        const options ={
+            body: addressId
+        };
+        $http.delete($scope.prefix + '/user/billing/' + addressId)
+            .then(function () {
+                $scope.getUserInfo();
+            },function (response){
+                alert(response.status);
+            });
+    }
+
+    $scope.getUserInfo();
+
+});
